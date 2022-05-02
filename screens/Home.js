b7b7e01f03cbb16cb5e18b73515a74baa45d70ea
Noah Valuks <noahvaluks@Noahs-MacBook-Pro.local>
2022-05-02 09:30:44 +0100

    course finished

--- screens/Home.js
+++ screens/Home.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { FlatList, StyleSheet} from 'react-native';
+import { FlatList, StyleSheet, TouchableOpacity, Text} from 'react-native';
 import { useCallback, useEffect, useState } from 'react';
 import PalettePreview from '../components/PalettePreview';
 
 
-const Home = ({navigation}) => {
+const Home = ({ navigation, route }) => {
 
+    const newPalette = route.params ? route.params.newPalette : null;
     const [colorPalettes, setColorPalettes] = useState([]);
     const [isRefreshing, setIsRefreshing] = useState(false);
 
@@ -30,14 +31,25 @@ const Home = ({navigation}) => {
         // setIsRefreshing(false);
     }, [])
 
+    useEffect(() => {
+        if (newPalette) {
+          setColorPalettes(current => [newPalette, ...current]);
+        }
+      }, [newPalette]);
+
     return (
            <FlatList
            style={styles.list}
             data={colorPalettes}
             keyExtractor={item => item.id}
             renderItem={({ item }) => (
-                <PalettePreview handlePress={() => {navigation.navigate('Color Palette', item)}} colorPalette={item} />
+                <PalettePreview handlePress={() => {navigation.navigate('ColorPalette', item)}} colorPalette={item} />
             )} refreshing={isRefreshing} onRefresh={handleRefresh}
+            ListHeaderComponent={
+                <TouchableOpacity style={styles.button} onPress={() => {navigation.navigate('ColorPaletteModal')}}>
+                    <Text style={styles.buttonText}>Add a Color Scheme</Text>
+                </TouchableOpacity>
+            }
             />         
    )
 };
@@ -46,6 +58,21 @@ const styles = StyleSheet.create({
     list: {
         padding: 10,
         backgroundColor: 'white',
+    },
+    button: {
+        borderColor: '#53777A',
+        borderWidth: 1,
+        padding: 5,
+        backgroundColor: '#7FC7AF',
+        borderRadius: 5,
+        marginBottom: 10,
+    },
+    buttonText: {
+        color: '#53777A',
+        fontWeight: 'bold',
+        paddingBottom: 6,
+        fontSize: 35,
+        textAlign: 'center'
     }
 })
 
